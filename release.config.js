715ac97f0c448bd41ab8e2712c577f71521234bb
Zaob <ngonhuthanhtrung1409@gmail.com>
2026-01-21 14:26:49 +0700

    fix(ci): remove [skip ci] from release commits to trigger GitHub Action

--- release.config.js
+++ release.config.js
@@ -1,67 +1,65 @@
 module.exports = {
   branches: [
-    "main",
+    'main',
     {
-      name: "beta",
+      name: 'beta',
       prerelease: true,
     },
   ],
   plugins: [
     [
-      "@semantic-release/commit-analyzer",
+      '@semantic-release/commit-analyzer',
       {
-        preset: "angular",
+        preset: 'angular',
         releaseRules: [
-          { type: "feat", release: "minor" },
-          { type: "fix", release: "patch" },
-          { type: "perf", release: "patch" },
-          { type: "refactor", release: false },
-          { type: "test", release: false },
-          { type: "docs", release: false },
-          { type: "chore", release: false },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'refactor', release: false },
+          { type: 'test', release: false },
+          { type: 'docs', release: false },
+          { type: 'chore', release: false },
         ],
       },
     ],
     [
-      "@semantic-release/release-notes-generator",
+      '@semantic-release/release-notes-generator',
       {
-        preset: "angular",
+        preset: 'angular',
         writerOpts: {
-          commitsSort: "subject",
+          commitsSort: 'subject',
         },
       },
     ],
     [
-      "@semantic-release/changelog",
+      '@semantic-release/changelog',
       {
-        changelogFile: "CHANGELOG.md",
+        changelogFile: 'CHANGELOG.md',
       },
     ],
     [
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
-        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
       },
     ],
     [
-      "@semantic-release/github",
+      '@semantic-release/github',
       {
         assets: [
           {
-            path: "dist",
-            label: "Distribution files",
+            path: 'dist',
+            label: 'Distribution files',
           },
         ],
       },
     ],
     [
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
-        assets: ["CHANGELOG.md", "package.json", "dist"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        assets: ['CHANGELOG.md', 'package.json', 'dist'],
+        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
       },
     ],
   ],
